6f8c7a1043b1d751d2aa5b5ecf661e6390bf6109
edmilson-dk <droidvidaboa@gmail.com>
2021-06-09 17:56:29 -0300

    feat: created books dto

---
+++ sequelize/sequelize-with-typescript-and-express/src/dtos/book/index.ts
@@ -0,0 +1,18 @@
+export type AddBookDto = {
+  title: string;
+  author: string;
+  released: string;
+  pages_total: number;
+  image_name: string;
+}
+
+export type BookAtributtes = {
+  title: string;
+  author: string;
+  released: string;
+  pages_total: number;
+  image_name: string;
+  id: string;
+  created_at: string;
+  updated_at: string;
+}
