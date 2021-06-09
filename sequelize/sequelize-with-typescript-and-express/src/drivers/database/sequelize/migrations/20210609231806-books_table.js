b57a80733e58be2ad26c83fa28bcade5fc6331eb
edmilson-dk <droidvidaboa@gmail.com>
2021-06-09 20:35:20 -0300

    feat: created new books migration

---
+++ sequelize/sequelize-with-typescript-and-express/src/drivers/database/sequelize/migrations/20210609231806-books_table.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('books', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.DataTypes.UUID,
+        defaultValue: Sequelize.DataTypes.UUIDV4,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.DataTypes.UUID,
+        references: {
+          model: {
+            tableName: 'users',
+          },
+          key: 'id'
+        },
+      },
+      title: { 
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      author: { 
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      released: { 
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      pages_total: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      image_name: { 
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      likes: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      dislikes: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: new Date(),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: new Date(),
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('books');
+  }
+};
