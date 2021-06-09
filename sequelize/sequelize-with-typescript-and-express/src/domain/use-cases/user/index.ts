fb57bbda55fbd0eebf2e23c9a3a4d7b345edae1e
edmilson-dk <droidvidaboa@gmail.com>
2021-06-09 20:58:09 -0300

    feat: added new user use cases return type

--- sequelize/sequelize-with-typescript-and-express/src/domain/use-cases/user/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/domain/use-cases/user/index.ts
@@ -2,5 +2,5 @@ import { AddUserDto, StoredUserDto } from "src/dtos/user";
 
 export interface IUserUseCases {
   addUser: (data: AddUserDto) => Promise<void>;
-  findUser: (userId: string) => Promise<StoredUserDto | []>;
+  findUser: (userId: string) => Promise<StoredUserDto | null>;
 }
