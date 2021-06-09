b8a5385f155f7c9f34bad87bb4473ccfef0c3744
edmilson-dk <droidvidaboa@gmail.com>
2021-06-09 20:36:06 -0300

    refactor: removed comments and spaces

--- sequelize/sequelize-with-typescript-and-express/src/models/book/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/models/book/index.ts
@@ -1,4 +1,4 @@
-import { Model, DataTypes } from "sequelize/types";
+import { Model, DataTypes } from "sequelize";
 
 import db from "src/drivers/database/sequelize";
 import { AddBookDto, StoredBookDto } from "src/dtos/book";
