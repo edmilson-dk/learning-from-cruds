3b5719754419a1909c401370398ade645609e643
edmilson-dk <droidvidaboa@gmail.com>
2021-06-09 21:20:32 -0300

    feat: created find user by email in user repository interface

--- sequelize/sequelize-with-typescript-and-express/src/application/repositories/user/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/application/repositories/user/index.ts
@@ -3,4 +3,5 @@ import { AddUserDto, StoredUserDto } from "src/dtos/user";
 export interface IUserRepository {
   addUser: (data: AddUserDto) => Promise<void>;
   findUser: (userId: string) => Promise<StoredUserDto | null>;
+  findUserByEmail: (email: string) => Promise<StoredUserDto | null>;
 }
