4376e859f4df1017de6397b57bcc12404a1eb3f1
edmilson-dk <droidvidaboa@gmail.com>
2021-06-10 22:13:27 -0300

    feat: created sharp disk storage image class methods

---
+++ sequelize/sequelize-with-typescript-and-express/src/drivers/image-upload/sharp/index.ts
@@ -0,0 +1,33 @@
+import fs from "fs";
+import sharp from "sharp";
+import { Express } from "express";
+
+import { IDiskImageStorage } from "src/application/repositories/disk-image-storage";
+
+export class SharpDiskImageStorage implements IDiskImageStorage {
+  async resizeImage(file: Express.Multer.File, size: number): Promise<Buffer> {
+    return sharp(file.path)
+      .resize(size)
+      .toFormat("webp")
+      .webp({ quality: 80 })
+      .toBuffer();
+  }
+
+  saveImage(imageBuffer: Buffer, file: Express.Multer.File): string {
+    const newFilename = file.filename.split(".")[0] + ".webp";
+    const pathArray = file.path.split("/");
+    pathArray.pop();
+    const newPath = `${pathArray.join("/")}/resized/${newFilename}`;
+
+    fs.access(file.path, (err) => {
+      if (!err) 
+        fs.unlink(file.path, err => { if (err) console.log(err) });
+    });
+        
+    fs.writeFile(newPath, imageBuffer, err => {
+      if(err) throw err;
+    });
+
+    return newFilename;
+  }
+}
