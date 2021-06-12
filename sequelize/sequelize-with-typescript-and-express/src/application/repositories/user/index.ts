4b013596f15ce7c24e1a95b02c8b3b71f040c909
edmilson-dk <droidvidaboa@gmail.com>
2021-06-11 22:57:46 -0300

    feat: added new user repository interface method return type

--- sequelize/sequelize-with-typescript-and-express/src/application/repositories/user/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/application/repositories/user/index.ts
@@ -1,7 +1,7 @@
-import { AddUserDto, StoredUserDto } from "src/domain/dtos/user";
+import { AddUserDto, PublicUserDto, StoredUserDto } from "src/domain/dtos/user";
 
 export interface IUserRepository {
-  addUser: (data: AddUserDto) => Promise<void>;
+  addUser: (data: AddUserDto) => Promise<PublicUserDto>;
   findUser: (userId: string) => Promise<StoredUserDto | null>;
   findUserByEmail: (email: string) => Promise<StoredUserDto | null>;
 }
