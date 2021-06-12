818c720b9c2d92388088ab93b255471cf4f477cc
edmilson-dk <droidvidaboa@gmail.com>
2021-06-11 22:56:15 -0300

    feat: created add user controller

--- sequelize/sequelize-with-typescript-and-express/src/adapters/presentation/controllers/user/add-user-controller.ts
+++ sequelize/sequelize-with-typescript-and-express/src/adapters/presentation/controllers/user/add-user-controller.ts
@@ -1,32 +1,52 @@
+import { IDiskImageStorage } from "src/application/repositories/disk-image-storage";
 import { IUserUseCases } from "src/domain/use-cases/user";
 import { encryptData } from "src/infra/security/bcrypt";
-import { ok, serverError } from "../../http/http-response-type";
+import { createJWT } from "src/infra/security/jwt";
+import { badRequest, ok, serverError } from "../../http/http-response-type";
 import { HttpRequest, HttpResponse } from "../../http/ports/http";
 import { BaseController } from "../baseControler";
 
 export class AddUserController implements BaseController {
   private readonly userServices: IUserUseCases;
+  private readonly diskImageStorage: IDiskImageStorage;
 
-  constructor(userServices: IUserUseCases) {
+  constructor(userServices: IUserUseCases, diskImageStorage: IDiskImageStorage) {
     this.userServices = userServices;
+    this.diskImageStorage = diskImageStorage;
   }
 
   async execute(httpRequest: HttpRequest): Promise<HttpResponse> {
-    try {
-      const { avatar, bio, email, name, password, } = httpRequest.body;
+    const { filename } = httpRequest.rest.file;
 
+    try {
+      const { bio, email, name, password } = httpRequest.body;
+
+      if (!filename) {
+        return badRequest(new Error("Image is missing"), 401);
+      }
+
+      const [ originaFileName ] = filename.split('.');
+      const imageName = `${originaFileName}.webp`;
+  
       const hashedPassword = await encryptData(password, 10);
 
       const data = await this.userServices.addUser({
-        avatar,
+        avatar: imageName,
         bio,
         email,
         name,
         password: hashedPassword,
-      })
+      });
 
-      return ok({}, 201);
+      const imageBuffer = await this.diskImageStorage.resizeImage(httpRequest.rest.file, 300);
+      await this.diskImageStorage.saveImage(imageBuffer, httpRequest.rest.file);
+      this.diskImageStorage.deleteNotResizedImage(filename);
+
+      const token = createJWT(email, data.id, "3d");
+
+      return ok({...data, token}, 201);
     } catch (err) {
+      this.diskImageStorage.deleteNotResizedImage(filename);
       return serverError("Interval server error");
     }
   }
