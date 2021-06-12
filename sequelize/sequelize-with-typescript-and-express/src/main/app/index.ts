fb0e93e26e330cc57cbf133646b9b1cd3b997702
edmilson-dk <droidvidaboa@gmail.com>
2021-06-12 19:09:50 -0300

    feat: added express static files routes

--- sequelize/sequelize-with-typescript-and-express/src/main/app/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/main/app/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import path from "path";
 
 import { routes } from "../routes";
 
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/images/user", express.static(path.resolve(__dirname, '..', '..', '..', 'uploads', 'resized', 'user')));
+app.use("/images/book", express.static(path.resolve(__dirname, '..', '..', '..', 'uploads', 'resized', 'book')));
 
 app.use("/api", routes);
 
