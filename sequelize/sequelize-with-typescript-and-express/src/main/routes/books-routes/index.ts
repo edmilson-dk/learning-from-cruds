15ac0ac92862fcaadde088850bd9e343cf42fd7c
edmilson-dk <droidvidaboa@gmail.com>
2021-06-14 12:42:53 -0300

    feat: created get all books router

--- sequelize/sequelize-with-typescript-and-express/src/main/routes/books-routes/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/main/routes/books-routes/index.ts
@@ -3,10 +3,11 @@ const multer = require("multer");
 
 import { multerConfig } from "src/drivers/image-upload/multer";
 import { adaptRoute } from "src/main/adapters/express-adapter";
-import { makeAddBookFactory } from "src/main/factories/book";
+import { makeAddBookFactory, makeGetAllBooksFactory } from "src/main/factories/book";
 
 export const booksRoutes = Router();
 
 const upload = multer(multerConfig);
 
 booksRoutes.post("/book/create", upload.single("image"), adaptRoute(makeAddBookFactory()));
+booksRoutes.get("/book/all", adaptRoute(makeGetAllBooksFactory()));
