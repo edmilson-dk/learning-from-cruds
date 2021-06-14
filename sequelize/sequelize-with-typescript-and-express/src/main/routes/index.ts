74d44d941903097c9163ada0ffd5d60cd8bfe062
edmilson-dk <droidvidaboa@gmail.com>
2021-06-14 11:44:04 -0300

    feat: added user auth middleware

--- sequelize/sequelize-with-typescript-and-express/src/main/routes/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/main/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import authMiddleware from "../middlewares/routes/auth-user-token-middleware";
 
 import { booksRoutes } from "./books-routes";
 import { userRoutes } from "./user-routes";
 
 export const routes = Router();
 
-routes.use(booksRoutes);
-routes.use(userRoutes);
+routes.use("/session", authMiddleware);
+
+routes.use("/session", booksRoutes);
+routes.use("/user", userRoutes);
