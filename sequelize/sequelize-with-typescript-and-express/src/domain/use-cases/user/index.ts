e9dea50a5fe19d4770b22fcc3080fb7f0620c6ae
edmilson-dk <droidvidaboa@gmail.com>
2021-06-13 21:27:14 -0300

    refactor: added new getUser method

--- sequelize/sequelize-with-typescript-and-express/src/domain/use-cases/user/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/domain/use-cases/user/index.ts
@@ -2,5 +2,5 @@ import { AddUserDto, PublicUserDto, StoredUserDto } from "src/domain/dtos/user";
 
 export interface IUserUseCases {
   addUser: (data: AddUserDto) => Promise<PublicUserDto>;
-  findUser: (userId: string) => Promise<StoredUserDto | null>;
+  getUser: (email: string, password: string) => Promise<StoredUserDto | null>;
 }
