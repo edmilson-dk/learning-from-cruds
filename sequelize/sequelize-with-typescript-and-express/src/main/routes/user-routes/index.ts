52f42f660f9fad854410e2ab675681ecc4fbf6ce
edmilson-dk <droidvidaboa@gmail.com>
2021-06-14 11:43:47 -0300

    feat: added new user routes path name

--- sequelize/sequelize-with-typescript-and-express/src/main/routes/user-routes/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/main/routes/user-routes/index.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
+const multer = require("multer");
+
 import { multerConfig } from "src/drivers/image-upload/multer";
 import { adaptRoute } from "src/main/adapters/express-adapter";
 import { makeAddUserFactory } from "src/main/factories/user";
 import { makeGetUserFactory } from "src/main/factories/user/get-user-factory";
-const multer = require("multer");
 
 export const userRoutes = Router();
 
 const upload = multer(multerConfig);
 
-userRoutes.post("/user/register", upload.single("image"), adaptRoute(makeAddUserFactory()));
-userRoutes.post("/user/login", adaptRoute(makeGetUserFactory()));
+userRoutes.post("/register", upload.single("image"), adaptRoute(makeAddUserFactory()));
+userRoutes.post("/login", adaptRoute(makeGetUserFactory()));
