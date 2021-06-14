87b0515faf323bd6689f9b95769d5bb437e23ef9
edmilson-dk <droidvidaboa@gmail.com>
2021-06-13 21:39:23 -0300

    feat: created login user router

--- sequelize/sequelize-with-typescript-and-express/src/main/routes/user-routes/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/main/routes/user-routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { multerConfig } from "src/drivers/image-upload/multer";
 import { adaptRoute } from "src/main/adapters/express-adapter";
 import { makeAddUserFactory } from "src/main/factories/user";
+import { makeGetUserFactory } from "src/main/factories/user/get-user-factory";
 const multer = require("multer");
 
 export const userRoutes = Router();
 
 const upload = multer(multerConfig);
 
-userRoutes.post("/user/add", upload.single("image"), adaptRoute(makeAddUserFactory()));
+userRoutes.post("/user/register", upload.single("image"), adaptRoute(makeAddUserFactory()));
+userRoutes.post("/user/login", adaptRoute(makeGetUserFactory()));
