d2d0a6e72354ae5e88c202acab9a9a7e24bb2ee3
edmilson-dk <droidvidaboa@gmail.com>
2021-06-14 11:44:53 -0300

    feat: created add book router

--- sequelize/sequelize-with-typescript-and-express/src/main/routes/books-routes/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/main/routes/books-routes/index.ts
@@ -1,3 +1,12 @@
 import { Router } from "express";
+const multer = require("multer");
+
+import { multerConfig } from "src/drivers/image-upload/multer";
+import { adaptRoute } from "src/main/adapters/express-adapter";
+import { makeAddBookFactory } from "src/main/factories/book";
 
 export const booksRoutes = Router();
+
+const upload = multer(multerConfig);
+
+booksRoutes.post("/book/create", upload.single("image"), adaptRoute(makeAddBookFactory()));
