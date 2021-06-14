eeb96a36d6df4812f6509c718f9ca0bfcaa03457
edmilson-dk <droidvidaboa@gmail.com>
2021-06-13 22:09:22 -0300

    feat: created is valid login user data

--- sequelize/sequelize-with-typescript-and-express/src/domain/validations/user/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/domain/validations/user/index.ts
@@ -7,6 +7,11 @@ type RegisterUserData = {
   bio: string;
 }
 
+type LoginUserData = {
+  password: string;
+  email: string;
+}
+
 export function isInvalidRegisterUserData({ name, email, password, bio }: RegisterUserData) {
   const schema = Joi.object<RegisterUserData>({
     name: Joi.string().min(3).max(40).required(),
@@ -14,9 +19,19 @@ export function isInvalidRegisterUserData({ name, email, password, bio }: Regist
     bio: Joi.string().min(10).max(255).required(),
     password: Joi.string().min(8).max(36)
   });
-
   
   const result = schema.validate({ name, email, password, bio });
+
+  return result.error ? result.error : false;
+}
+
+export function isInvalidLoginUserData({ email, password }: LoginUserData) {
+  const schema = Joi.object<RegisterUserData>({
+    email: Joi.string().email().regex(new RegExp( /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/)).required(),
+    password: Joi.string().min(8).max(36)
+  });
+  
+  const result = schema.validate({ email, password });
 
   return result.error ? result.error : false;
 }
