129eebe40a4aeb21389f39e41388ee5c6e1f6b69
edmilson-dk <droidvidaboa@gmail.com>
2021-06-14 10:29:20 -0300

    feat: created public user book dto

--- sequelize/sequelize-with-typescript-and-express/src/domain/dtos/book/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/domain/dtos/book/index.ts
@@ -9,6 +9,24 @@ export type AddBookDto = {
   user_id: string;
 }
 
+export type PublicBookDto = {
+  title: string;
+  author: string;
+  released: string;
+  pages_total: number;
+  image_name: string;
+  likes: number;
+  dislikes: number;
+  user_id: string;
+  id: string;
+}
+
+export type PublicUserBookDto = {
+  avatar: string;
+  name: string;
+  books: PublicBookDto[],
+}
+
 export type StoredBookDto = {
   id: string;
   created_at: string;
