de6a298ff99d0ed1ce6e806ed12881fa2e27f393
edmilson-dk <droidvidaboa@gmail.com>
2021-06-14 10:26:50 -0300

    feat: created book mapper

---
+++ sequelize/sequelize-with-typescript-and-express/src/domain/mappers/book/index.ts
@@ -0,0 +1,25 @@
+import { PublicBookDto, PublicUserBookDto } from "src/domain/dtos/book";
+
+export class BookMapper {
+  static toPublicDto(data: any): PublicBookDto {
+    return {
+      user_id: data.user_id,
+      id: data.id,
+      title: data.title,
+      author: data.author,
+      released: data.released,
+      pages_total: data.pages_total,
+      image_name: data.image_name,
+      likes: data.likes,
+      dislikes: data.dislikes,
+    }
+  }
+
+  static toPublicUserDto(data: any): PublicUserBookDto {
+    return {
+      avatar: data.avatar,
+      name: data.name,
+      books: data.books,
+    }
+  }
+}
