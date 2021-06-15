825b331027f926fd0541de5eac5e361770f3af1c
edmilson-dk <droidvidaboa@gmail.com>
2021-06-15 10:28:30 -0300

    feat: created get one book router

--- sequelize/sequelize-with-typescript-and-express/src/main/routes/books-routes/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/main/routes/books-routes/index.ts
@@ -4,7 +4,7 @@ const multer = require("multer");
 import { multerConfig } from "src/drivers/image-upload/multer";
 import { RedisDataCache } from "src/infra/repositories/data-cache/redis";
 import { adaptRoute } from "src/main/adapters/express-adapter";
-import { makeAddBookFactory, makeGetAllBooksFactory } from "src/main/factories/book";
+import { makeAddBookFactory, makeGetAllBooksFactory, makeGetOneBookFactory } from "src/main/factories/book";
 
 export const booksRoutes = Router();
 
@@ -14,3 +14,4 @@ const dataCacheRepository = new RedisDataCache();
 
 booksRoutes.post("/book/create", upload.single("image"), adaptRoute(makeAddBookFactory(dataCacheRepository)));
 booksRoutes.get("/book/all", adaptRoute(makeGetAllBooksFactory(dataCacheRepository)));
+booksRoutes.get("/book/:bookId", adaptRoute(makeGetOneBookFactory(dataCacheRepository)));
