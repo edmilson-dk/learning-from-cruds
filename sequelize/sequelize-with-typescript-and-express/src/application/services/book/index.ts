0a681fa7f533bb611ad11b49d9cf832412f8b305
edmilson-dk <droidvidaboa@gmail.com>
2021-06-14 22:11:14 -0300

    feat: added data cache repository property

--- sequelize/sequelize-with-typescript-and-express/src/application/services/book/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/application/services/book/index.ts
@@ -1,13 +1,17 @@
 import { IBookRepository } from "src/application/repositories/book";
+import { IDataCacheRepository } from "src/application/repositories/data-cache";
 import { AddBookDto, PublicBookDto, PublicUserBookDto } from "src/domain/dtos/book";
+import { BookMapper } from "src/domain/mappers/book";
 import { IBookUseCases } from "src/domain/use-cases/book";
 import { isInvalidBookCreateData } from "src/domain/validations/book";
 
 export class BookServices implements IBookUseCases {
   private readonly bookRepository: IBookRepository;
+  private readonly dataCacheRepository: IDataCacheRepository;
 
-  constructor(bookRepository: IBookRepository) {
+  constructor(bookRepository: IBookRepository, dataCacheRepository: IDataCacheRepository) {
     this.bookRepository = bookRepository;
+    this.dataCacheRepository = dataCacheRepository;
   }
 
   async addBook(data: AddBookDto): Promise<PublicBookDto> {
@@ -27,7 +31,17 @@ export class BookServices implements IBookUseCases {
   }
 
   async getAllBooks(userId: string): Promise<PublicUserBookDto> {
+    const cached = await this.dataCacheRepository.getCache("books");
+
+    if (cached) {
+      console.log("cache")
+      return BookMapper.toPublicUserDto(cached);
+    }
+
+    console.log("database")
     const books = await this.bookRepository.getAllBooks(userId);
+    this.dataCacheRepository.setCache("books", books, 20);
+
     return books;
   }
 }
