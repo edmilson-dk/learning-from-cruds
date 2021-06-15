e282d815fedfaae78f3637b76dba318d71770726
edmilson-dk <droidvidaboa@gmail.com>
2021-06-15 11:43:06 -0300

    feat: created book repository interface news methods

--- sequelize/sequelize-with-typescript-and-express/src/application/repositories/book/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/application/repositories/book/index.ts
@@ -1,7 +1,9 @@
-import { AddBookDto, PublicBookDto, PublicUserBookDto } from "src/domain/dtos/book";
+import { AddBookDto, PublicBookDto, PublicUserBookDto, UpdateBookDto } from "src/domain/dtos/book";
 
 export interface IBookRepository {
   addBook: (data: AddBookDto) => Promise<PublicBookDto>;
   getOneBook: (userId: string, bookId: string) => Promise<PublicUserBookDto>;
   getAllBooks: (userId: string) => Promise<PublicUserBookDto>;
+  deleteBook: (userId: string, bookId: string) => Promise<boolean>;
+  updateBook: (userId: string, bookId: string, data: UpdateBookDto) => Promise<boolean>; 
 }
