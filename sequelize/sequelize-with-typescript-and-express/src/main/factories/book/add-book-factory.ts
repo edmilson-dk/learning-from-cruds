18855c57160a966c9010dda34746b0de02bcc7c0
edmilson-dk <droidvidaboa@gmail.com>
2021-06-14 22:11:30 -0300

    feat: added data cache repository property

--- sequelize/sequelize-with-typescript-and-express/src/main/factories/book/add-book-factory.ts
+++ sequelize/sequelize-with-typescript-and-express/src/main/factories/book/add-book-factory.ts
@@ -1,12 +1,13 @@
 import { BaseController } from "src/adapters/presentation/controllers/baseControler";
 import { AddBookController } from "src/adapters/presentation/controllers/book";
+import { IDataCacheRepository } from "src/application/repositories/data-cache";
 import { BookServices } from "src/application/services/book";
 import { SharpDiskImageStorage } from "src/drivers/image-upload/sharp";
 import { BookSequelizePgRepository } from "src/infra/repositories/sequelize/pg/book";
 
-export function makeAddBookFactory(): BaseController {
+export function makeAddBookFactory(dataCacheRepository: IDataCacheRepository): BaseController {
   const bookRepository = new BookSequelizePgRepository();
-  const bookServices = new BookServices(bookRepository);
+  const bookServices = new BookServices(bookRepository, dataCacheRepository);
   const diskImageStorage =new SharpDiskImageStorage();
   const controller = new AddBookController(bookServices, diskImageStorage);
 
--- sequelize/sequelize-with-typescript-and-express/src/main/factories/book/get-all-books-factory.ts
+++ sequelize/sequelize-with-typescript-and-express/src/main/factories/book/get-all-books-factory.ts
@@ -1,11 +1,12 @@
 import { BaseController } from "src/adapters/presentation/controllers/baseControler";
 import { GetAllBooksController } from "src/adapters/presentation/controllers/book";
+import { IDataCacheRepository } from "src/application/repositories/data-cache";
 import { BookServices } from "src/application/services/book";
 import { BookSequelizePgRepository } from "src/infra/repositories/sequelize/pg/book";
 
-export function makeGetAllBooksFactory(): BaseController {
+export function makeGetAllBooksFactory(dataCacheRepository: IDataCacheRepository): BaseController {
   const bookRepository = new BookSequelizePgRepository();
-  const bookServices = new BookServices(bookRepository);
+  const bookServices = new BookServices(bookRepository, dataCacheRepository);
   const controller = new GetAllBooksController(bookServices);
 
   return controller;
