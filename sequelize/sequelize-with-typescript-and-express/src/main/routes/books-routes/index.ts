f4094df589d7693a9f6e338473b72c7a29f55643
edmilson-dk <droidvidaboa@gmail.com>
2021-06-14 22:11:44 -0300

    feat: created redis cache repository instance

--- sequelize/sequelize-with-typescript-and-express/src/main/routes/books-routes/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/main/routes/books-routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 const multer = require("multer");
 
 import { multerConfig } from "src/drivers/image-upload/multer";
+import { RedisDataCache } from "src/infra/repositories/data-cache/redis";
 import { adaptRoute } from "src/main/adapters/express-adapter";
 import { makeAddBookFactory, makeGetAllBooksFactory } from "src/main/factories/book";
 
@@ -9,5 +10,7 @@ export const booksRoutes = Router();
 
 const upload = multer(multerConfig);
 
-booksRoutes.post("/book/create", upload.single("image"), adaptRoute(makeAddBookFactory()));
-booksRoutes.get("/book/all", adaptRoute(makeGetAllBooksFactory()));
+const dataCacheRepository = new RedisDataCache();
+
+booksRoutes.post("/book/create", upload.single("image"), adaptRoute(makeAddBookFactory(dataCacheRepository)));
+booksRoutes.get("/book/all", adaptRoute(makeGetAllBooksFactory(dataCacheRepository)));
