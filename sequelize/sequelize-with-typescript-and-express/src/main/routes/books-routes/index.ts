1f527d4ae0564bafe6b319198e841bef5e4e6c11
edmilson-dk <droidvidaboa@gmail.com>
2021-06-15 16:59:46 -0300

    feat: created delete book router

--- sequelize/sequelize-with-typescript-and-express/src/main/routes/books-routes/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/main/routes/books-routes/index.ts
@@ -3,8 +3,10 @@ const multer = require("multer");
 
 import { multerConfig } from "src/drivers/image-upload/multer";
 import { RedisDataCache } from "src/infra/repositories/data-cache/redis";
+import { BookSequelizePgRepository } from "src/infra/repositories/sequelize/pg/book";
 import { adaptRoute } from "src/main/adapters/express-adapter";
 import { makeAddBookFactory, makeGetAllBooksFactory, makeGetOneBookFactory } from "src/main/factories/book";
+import { makeDeleteBookFactory } from "src/main/factories/book/delete-book-factory";
 
 export const booksRoutes = Router();
 
@@ -15,3 +17,4 @@ const dataCacheRepository = new RedisDataCache();
 booksRoutes.post("/book/create", upload.single("image"), adaptRoute(makeAddBookFactory(dataCacheRepository)));
 booksRoutes.get("/book/all", adaptRoute(makeGetAllBooksFactory(dataCacheRepository)));
 booksRoutes.get("/book/:bookId", adaptRoute(makeGetOneBookFactory(dataCacheRepository)));
+booksRoutes.delete("/book/:bookId", adaptRoute(makeDeleteBookFactory(dataCacheRepository)));
