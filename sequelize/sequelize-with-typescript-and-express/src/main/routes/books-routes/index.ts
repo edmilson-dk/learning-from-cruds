63662c3f94ad003426fa8db4ce5a13447915e354
edmilson-dk <droidvidaboa@gmail.com>
2021-06-15 21:59:33 -0300

    feat: created update book router

--- sequelize/sequelize-with-typescript-and-express/src/main/routes/books-routes/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/main/routes/books-routes/index.ts
@@ -5,7 +5,7 @@ import { multerConfig } from "src/drivers/image-upload/multer";
 import { RedisDataCache } from "src/infra/repositories/data-cache/redis";
 import { BookSequelizePgRepository } from "src/infra/repositories/sequelize/pg/book";
 import { adaptRoute } from "src/main/adapters/express-adapter";
-import { makeAddBookFactory, makeGetAllBooksFactory, makeGetOneBookFactory } from "src/main/factories/book";
+import { makeAddBookFactory, makeGetAllBooksFactory, makeGetOneBookFactory, makeUpdateBookFactory } from "src/main/factories/book";
 import { makeDeleteBookFactory } from "src/main/factories/book/delete-book-factory";
 
 export const booksRoutes = Router();
@@ -18,3 +18,4 @@ booksRoutes.post("/book/create", upload.single("image"), adaptRoute(makeAddBookF
 booksRoutes.get("/book/all", adaptRoute(makeGetAllBooksFactory(dataCacheRepository)));
 booksRoutes.get("/book/:bookId", adaptRoute(makeGetOneBookFactory(dataCacheRepository)));
 booksRoutes.delete("/book/:bookId", adaptRoute(makeDeleteBookFactory(dataCacheRepository)));
+booksRoutes.put("/book/:bookId", adaptRoute(makeUpdateBookFactory(dataCacheRepository)));
