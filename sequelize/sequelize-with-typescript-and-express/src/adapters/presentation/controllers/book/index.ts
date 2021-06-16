25a601e32212b90de863c63cb0692b88f7878faa
edmilson-dk <droidvidaboa@gmail.com>
2021-06-15 22:00:08 -0300

    feat: added export update book controller

--- sequelize/sequelize-with-typescript-and-express/src/adapters/presentation/controllers/book/index.ts
+++ sequelize/sequelize-with-typescript-and-express/src/adapters/presentation/controllers/book/index.ts
@@ -2,3 +2,4 @@ export * from "./add-book-controller";
 export * from "./get-all-books-controller";
 export * from "./get-one-book-controller";
 export * from "./delete-book-controller";
+export * from "./update-book-controller";
