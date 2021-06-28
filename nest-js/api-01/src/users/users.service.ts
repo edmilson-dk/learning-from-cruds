521a7523e55ce35dd51c36736197c6bc20289aec
edmilson-dk <droidvidaboa@gmail.com>
2021-06-28 11:12:21 -0300

    feat: added nest inject model

--- nest-js/api-01/src/users/users.service.ts
+++ nest-js/api-01/src/users/users.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
