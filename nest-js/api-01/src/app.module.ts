96c4097b2719c9fa01bc464a3cc5fbf6a05cea4b
edmilson-dk <droidvidaboa@gmail.com>
2021-06-28 11:08:41 -0300

    other: nestjs project default setup

--- nest-js/api-01/src/app.module.ts
+++ nest-js/api-01/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://dk:dk123456@cluster0.hjsen.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
